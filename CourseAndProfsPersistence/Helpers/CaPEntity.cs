66c7e43184fbbbaded4c9e427eec902df9a83ded
vagmark <vagmark30@gmail.com>
2021-04-24 18:28:04 +0300

    Added Helpers

---
+++ CourseAndProfsPersistence/Helpers/CaPEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kritikos.Configuration.Persistence.Abstractions;
+
+namespace CourseAndProfsPersistence.Helpers
+{
+  public class CaPEntity<TKey> : CaPJoinEntity, IEntity<TKey>
+    where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
+  {
+    public TKey Id { get; set; }
+  }
+}
---
+++ CourseAndProfsPersistence/Helpers/CaPJoinEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kritikos.Configuration.Persistence.Abstractions;
+
+
+namespace CourseAndProfsPersistence.Helpers
+{
+  public abstract class CaPJoinEntity : IAuditable<Guid>, ITimestamped
+  {
+    #region Implementation of IAuditable<Guid>
+
+    /// <inheritdoc />
+    public Guid CreatedBy { get; set; }
+
+    /// <inheritdoc />
+    public Guid UpdatedBy { get; set; }
+
+    #endregion
+    #region Implementation of ITimestamped
+
+    /// <inheritdoc />
+    public DateTimeOffset CreatedAt { get; set; }
+
+    /// <inheritdoc />
+    public DateTimeOffset UpdatedAt { get; set; }
+
+    #endregion
+  }
+}
