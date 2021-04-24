e1665edf9eb099e70c56cba942738c3dc249fd95
vagmark <vagmark30@gmail.com>
2021-04-24 18:32:14 +0300

    WIP Edited main services

--- CourseAndProfsClient/Startup.cs
+++ CourseAndProfsClient/Startup.cs
@@ -5,6 +5,39 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CourseAndProfsClient;
+//using CourseAndProfsClient.Identity;
+//using CourseAndProfsClient.Helpers;
+//using CourseAndProfsClient.Helpers.Extensions;
+//using CourseAndProfsClient.Options;
+
+using Kritikos.Configuration.Persistence.Extensions;
+using Kritikos.Configuration.Persistence.Interceptors;
+using Kritikos.Configuration.Persistence.Services;
+using Kritikos.PureMap;
+using Kritikos.PureMap.Contracts;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.Filters;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Text;
+using CourseAndProfsPersistence.Identity;
+using CourseAndProfsPersistence;
+using System;
 
 namespace CourseAndProfsClient
 {
@@ -18,15 +51,75 @@ namespace CourseAndProfsClient
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
+    public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
     {
-
       services.AddControllersWithViews();
-
-      // In production, the React files will be served from this directory
       services.AddSpaStaticFiles(configuration =>
       {
         configuration.RootPath = "ClientApp/build";
+      });
+
+      //services.AddSingleton<IPureMapper>(sp => new PureMapper(MappingConfiguration.Mapping));
+
+      services.AddHttpContextAccessor();
+      services.AddSingleton<TimestampSaveChangesInterceptor>();
+      services.AddSingleton<AuditSaveChangesInterceptor<Guid>>();
+
+
+      services.AddDbContextPool<CaPDbContext>((serviceProvider, options) =>
+      {
+        options.UseNpgsql(
+            Configuration.GetConnectionString("CaP"))
+          .AddInterceptors(
+            serviceProvider.GetRequiredService<TimestampSaveChangesInterceptor>(),
+            serviceProvider.GetRequiredService<AuditSaveChangesInterceptor<Guid>>())
+          .EnableCommonOptions(env);
+      });
+
+      services.AddDatabaseDeveloperPageExceptionFilter();
+      services.AddHostedService<MigrationService<CaPDbContext>>();
+
+
+
+
+
+      services.AddIdentity<CaPUser, CaPRoles>(c =>
+      {
+        var isDevelopment = env.IsDevelopment();
+        c.User.RequireUniqueEmail = !isDevelopment;
+        c.Password = new PasswordOptions
+        {
+          RequireDigit = !isDevelopment,
+          RequireLowercase = !isDevelopment,
+          RequireUppercase = !isDevelopment,
+          RequireNonAlphanumeric = !isDevelopment,
+          RequiredLength = isDevelopment
+            ? 4
+            : 6,
+        };
+      })
+        .AddEntityFrameworkStores<CaPDbContext>()
+        .AddDefaultUI()
+        .AddDefaultTokenProviders();
+
+      services.AddIdentityServer()
+        .AddApiAuthorization<CaPUser, CaPDbContext>();
+
+      
+
+      //services.AddCorrelation();
+      services.AddControllersWithViews();
+      services.AddRazorPages();
+
+      services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("AllowAll",
+          builder =>
+          {
+            builder.AllowAnyOrigin();
+          });
       });
     }
 
