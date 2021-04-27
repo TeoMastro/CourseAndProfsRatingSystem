06cd02a62621c46cbd253ebc32dc8f09a86053d9
vagmark <vagmark30@gmail.com>
2021-04-27 14:25:36 +0300

    Added Dto -WIP

---
+++ CourseAndProfsClientModels/Dto/ProfessorDto.cs
@@ -0,0 +1,20 @@
+﻿using CourseAndProfsPersistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseAndProfsClientModels.Dto
+{
+  public class ProfessorDto
+  {
+    public long Id { get; set; }
+    public string FullName { get; set; }
+    public string Mail { get; set; }
+    public string Phone { get; set; }
+    public string Office { get; set; }
+    public string EOffice { get; set; }
+    public Department Department { get; set; }
+  }
+}
---
+++ CourseAndProfsClientModels/GetDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CourseAndProfsClientModels
+{
+  using System;
+  using System.ComponentModel;
+  using System.ComponentModel.DataAnnotations;
+
+  public record AddProfessorDto(string FullName, string Mail, string Phone, string Office, string EOffice);
+}
