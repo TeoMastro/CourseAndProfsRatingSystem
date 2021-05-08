ffd45495dfc8fe2e27455d28caa1ce6ffa86e5e4
vagmark <vagmark30@gmail.com>
2021-05-08 21:30:49 +0300

    Added fields and a helper RevDto

--- CourseAndProfsClientModels/Dto/AddReviewDto.cs
+++ CourseAndProfsClientModels/Dto/AddReviewDto.cs
@@ -1,5 +1,4 @@
-﻿using CourseAndProfsPersistence.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,17 +6,12 @@ using System.Threading.Tasks;
 
 namespace CourseAndProfsClientModels.Dto
 {
-  public class ReviewDto
+  public class AddReviewDto
   {
-    public long ReviewId { get; set; }
     public long CourseId { get; set; }
-    public string CourseName { get; set; }
     public long ProfessorId { get; set; }
-    public string ProfessorName { get; set; }
-    public Guid UserId { get; set; }
     public double UsersSubjectScore { get; set; }
     public double Rating { get; set; }
     public string Comments { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
   }
 }
