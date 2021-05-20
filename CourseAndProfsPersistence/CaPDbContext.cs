4573c20818ed1d63885259daeafc972575723278
vagmark <vagmark30@gmail.com>
2021-05-21 02:33:45 +0300

    Update on context for ProfCourse join dbset ++ migrations

--- CourseAndProfsPersistence/CaPDbContext.cs
+++ CourseAndProfsPersistence/CaPDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using CourseAndProfsPersistence.Identity;
-//using CourseAndProfsPersistence.Joins;
+using CourseAndProfsPersistence.Joins;
 using CourseAndProfsPersistence.Models;
 
 using Kritikos.Configuration.Peristence.IdentityServer;
@@ -24,11 +24,26 @@ namespace CourseAndProfsPersistence
 
     public DbSet<Review> Reviews{ get; set; }
 
+    public DbSet<ProfessorCourse> ProfessorCourses { get; set; }
+
     public DbSet<Device> Devices { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
       base.OnModelCreating(builder);
+
+      builder.Entity<ProfessorCourse>(e =>
+      {
+        e.HasOne(pr => pr.Professor)
+         .WithMany(c => c.ProfessorCourses)
+         .HasForeignKey("ProfessorId")
+         .OnDelete(DeleteBehavior.Restrict);
+        e.HasOne(c => c.Course)
+         .WithMany()
+         .HasForeignKey("CourseId")
+         .OnDelete(DeleteBehavior.Restrict);
+        e.HasKey("ProfessorId", "CourseId");
+      });
     }
   }
 }
