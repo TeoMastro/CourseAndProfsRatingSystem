3f6e51856a61cdd33f23e692f474e155b1515fc0
vagmark <vagmark30@gmail.com>
2021-05-21 02:32:36 +0300

    Added join ProfCourse

---
+++ CourseAndProfsPersistence/Joins/ProfessorCourse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseAndProfsPersistence.Helpers;
+using CourseAndProfsPersistence.Models;
+
+namespace CourseAndProfsPersistence.Joins
+{
+  public class ProfessorCourse : CaPJoinEntity
+  {
+#nullable disable
+    public Professor Professor { get; set; }
+    public Course Course { get; set; }
+#nullable enable
+  }
+}
